679524bb03f073a4a5cd4dabd7aed1dfe0d7baba
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-07-24 19:37:06 +0530

    small fix in the link

--- src/pages/ClaimFormPopup.js
+++ src/pages/ClaimFormPopup.js
@@ -22,7 +22,7 @@ const ClaimFormPopup = ({ prize, onClose }) => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const checkRes = await fetch('http://manhar-backend.vercel.app/api/scratch/check', {
+      const checkRes = await fetch('https://manhar-backend.vercel.app/api/scratch/check', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ mobile: formData.mobile })
@@ -33,7 +33,7 @@ const ClaimFormPopup = ({ prize, onClose }) => {
         return;
       }
 
-      await fetch('http://manhar-backend.vercel.app/api/scratch/claim', {
+      await fetch('https://manhar-backend.vercel.app/api/scratch/claim', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)

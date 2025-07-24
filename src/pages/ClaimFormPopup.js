e150caba04575f9cde1894dcd2732eed779ee199
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-07-24 23:11:29 +0530

    solved

--- src/pages/ClaimFormPopup.js
+++ src/pages/ClaimFormPopup.js
@@ -22,7 +22,7 @@ const ClaimFormPopup = ({ prize, onClose }) => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const checkRes = await fetch('https://admin-backend-orcin-six.vercel.app/api/check', {
+      const checkRes = await fetch('https://admin-backend-orcin-six.vercel.app/api/scratch/check', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ mobile: formData.mobile })
@@ -33,7 +33,7 @@ const ClaimFormPopup = ({ prize, onClose }) => {
         return;
       }
 
-      await fetch('https://admin-backend-orcin-six.vercel.app/api/claim', {
+      await fetch('https://admin-backend-orcin-six.vercel.app/api/scratch/claim', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)

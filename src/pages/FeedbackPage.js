d988f1f84446c4a2042acdf28006a6eb07caa44e
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-07-28 15:44:51 +0530

    all issues solved

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -3,11 +3,9 @@ import './FeedbackPage.css';
 
 const FeedbackPage = () => {
   const [hovered, setHovered] = useState(null);
-  const [showSuccess, setShowSuccess] = useState(false);
 
-  const handleSubmit = () => {
-    setShowSuccess(true);
-    setTimeout(() => setShowSuccess(false), 3000);
+  const handleProceed = () => {
+    window.location.href = 'https://www.google.com/maps/place/Manhar+Shopping+Mall/@18.1151287,83.409783,16z/data=!4m16!1m9!3m8!1s0x3a3be55a8568beed:0xdf49dfe85fa3dfc!2sManhar+Shopping+Mall!8m2!3d18.1156761!4d83.4104265!9m1!1b1!16s%2Fg%2F11h1gm43v!3m5!1s0x3a3be55a8568beed:0xdf49dfe85fa3dfc!8m2!3d18.1156761!4d83.4104265!16s%2Fg%2F11h1gm43v?entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D';
   };
 
   return (
@@ -32,25 +30,19 @@ const FeedbackPage = () => {
 
         <div className="feedback-right">
           <iframe
-            title="Google Maps Review Page"
-            src="https://maps.app.goo.gl/bpyhYaTdrXhRxLJP6?g_st=com.google.maps.preview.copy"
+            title="Manhar Shopping Mall"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7584.115303690269!2d83.409783!3d18.1151287!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3be55a8568beed%3A0xdf49dfe85fa3dfc!2sManhar%20Shopping%20Mall!5e0!3m2!1sen!2sin!4v1753697543815!5m2!1sen!2sin"
             width="100%"
             height="100%"
             frameBorder="0"
-            allow="fullscreen"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
 
-      <button className="submit-btn" onClick={handleSubmit}>Submit</button>
-
-      {showSuccess && (
-        <div className="success-popup">
-          <div className="success-content">
-            <p>✅ Review submitted successfully!</p>
-          </div>
-        </div>
-      )}
+      <button className="proceed-btn" onClick={handleProceed}>Proceed</button>
     </div>
   );
 };
